2364f818829dbe201f7c010bf13e711b8efcec52
TomixUG <tomas.sousek1@gmail.com>
2023-05-05 01:27:06 +0200

    save data to SQLite database

--- src/downloader.js
+++ src/downloader.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const fsExtra = require('fs-extra')
 
+
 const { fetchLecture, fetchQuestion, fetchAsset } = require('./api')
 const { parseQuestionHtml, serialResolve } = require('./utils')
 
@@ -17,8 +18,12 @@ function downloadAsset(lectureDir, videoUrl, name) {
   })
 }
 
-function downloadLecture(lecture) {
-  const lectureDir = path.join(DATA_DIR, `lecture-${lecture.id}`)
+function downloadLecture(lecture, db) {
+  const lectureDir = path.join(DATA_DIR, `${lecture.id}`)
+
+  // add category entry
+  db.run(`INSERT INTO categories VALUES ('${lecture.id}', '${lecture.name}');`);
+
   if (fs.existsSync(lectureDir)) {
     fsExtra.removeSync(lectureDir)
   }
@@ -40,19 +45,34 @@ function downloadLecture(lecture) {
             assetPromiseFactories.push(() => downloadAsset(lectureDir, videoUrl, `${id}-${imageUrls + 1}`))
           }
 
-          return serialResolve(assetPromiseFactories).then((assets) => ({
-            id,
-            text,
-            code: question.Code,
-            correctAnswers: question.CorrectAnswers,
-            answers,
-            assets
-          }))
+          return serialResolve(assetPromiseFactories).then((assets) => {
+            // save the question and answer
+            // adding the lecture.id to make it unique, there can be the same qeuestion in different category
+            let filePath = assets.length === 0 ? null : `'${assets[0].filename}'`; // if there is no image write null
+
+            let questionCode = question.Code === "" ? id : question.Code; // rarely the questionCode isn't there so we jsut use the normal id
+            db.run(`INSERT INTO questions VALUES ('${questionCode}${lecture.id}', '${text}', ${filePath}, '${lecture.id}');`);
+
+            answers.forEach(function(answer) {
+              // check if the current answer is in the correctAnswers array
+              let isCorrect = question.CorrectAnswers.includes(answer.id) ? 1 : 0;
+              db.run(`INSERT INTO answers VALUES ('${answer.id}${lecture.id}', '${answer.text}', ${isCorrect}, '${questionCode}${lecture.id}');`);
+            });
+
+            return {
+              id,
+              text,
+              code: question.Code,
+              correctAnswers: question.CorrectAnswers,
+              answers,
+              assets
+            };
+          })
         })
     })
 
     return serialResolve(questionPromises).then((questions) => {
-      console.log(`Saving ${lecture.name}`)
+      console.log(`Saving ${lecture.name} `)
       questions.sort((a, b) => a.code.localeCompare(b.code))
       fs.writeFileSync(path.join(lectureDir, 'data.json'), JSON.stringify(questions, null, 2))
     })
--- src/index.js
+++ src/index.js
@@ -4,6 +4,37 @@ const { lectures } = require('./lectures')
 const { downloadLecture } = require('./downloader')
 const { serialResolve } = require('./utils')
 
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('data.db');
+
+// create tables
+db.run(`
+  CREATE TABLE categories (
+      id TEXT PRIMARY KEY,
+      name TEXT NOT NULL
+  );
+`);
+
+// id is a questionCode
+db.run(`
+  CREATE TABLE questions (
+      id TEXT PRIMARY KEY,
+      text TEXT NOT NULL,
+      image_url TEXT,
+      category_id TEXT NOT NULL,
+      FOREIGN KEY (category_id) REFERENCES categories (id)
+  );
+`);
+db.run(`
+  CREATE TABLE answers (
+      id TEXT PRIMARY KEY,
+      text TEXT NOT NULL,
+      is_correct BOOLEAN NOT NULL,
+      question_id TEXT NOT NULL,
+      FOREIGN KEY (question_id) REFERENCES questions (id)
+  );
+`);
+
 inquirer
   .prompt([
     {
@@ -17,7 +48,8 @@ inquirer
     }
   ])
   .then((results) => {
-    const promiseFns = results.lectures.map((lecture) => () => downloadLecture(lecture))
+    console.log(results);
+    const promiseFns = results.lectures.map((lecture) => () => downloadLecture(lecture, db))
     return serialResolve(promiseFns)
   })
   .catch((err) => {
