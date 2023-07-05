9cac3b923f218408c899bc5364ea0805653a4ff9
TomixUG <tomas.sousek1@gmail.com>
2023-07-05 20:45:51 +0200

    rename categories, add points

--- src/lectures.js
+++ src/lectures.js
@@ -1,48 +1,59 @@
 // http://etesty2.mdcr.cz/Home/Tests/ro
 const lectures = [
   {
-    name: 'Zákon č. 361/2000Sb. (Pojmy, povinnosti)',
-    id: 24
+    name: 'Pojmy, povinnosti',
+    id: 24,
+    points: 2
   },
   {
-    name: 'Zákon č. 361/2000Sb. (Jízda vozidly)',
-    id: 16
+    name: 'Jízda vozidly',
+    id: 16,
+    points: 2
   },
   {
-    name: 'Zákon č. 361/2000Sb. (Ostatní ustanovení)',
-    id: 25
+    name: 'Ostatní ustanovení',
+    id: 25,
+    points: 2
   },
   {
     name: 'Dopravní značky',
-    id: 14
+    id: 14,
+    points: 1
   },
   {
     name: 'Řešení dopravních situací',
-    id: 17
+    id: 17,
+    points: 4
   },
   {
     name: 'Zásady bezpečné jízdy [A]',
-    id: 23
+    id: 23,
+    points: 2
   },
   {
     name: 'Zásady bezpečné jízdy [B]',
-    id: 19
+    id: 19,
+    points: 2
   },
   {
     name: 'Zásady bezpečné jízdy [C,D]',
-    id: 18
+    id: 18,
+    points: 2
   },
   {
     name: 'Související předpisy',
-    id: 21
+    id: 21,
+    points: 2
   },
   {
     name: 'Podmínky provozu vozidel',
-    id: 22
+    id: 22,
+    points: 1
   },
   {
     name: 'Zdravotnická příprava',
-    id: 20
+    id: 20,
+    points: 1
   }
 ]
 
