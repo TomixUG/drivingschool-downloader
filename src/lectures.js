97228865ba1cbcc01d11b6f6db279fdb44d6a3d0
TomixUG <tomas.sousek1@gmail.com>
2023-07-08 00:07:13 +0200

    add a type to lectures

--- src/lectures.js
+++ src/lectures.js
@@ -3,57 +3,68 @@ const lectures = [
   {
     name: 'Pojmy, povinnosti',
     id: 24,
-    points: 2
+    points: 2,
+    type: ['all']
   },
   {
     name: 'Jízda vozidly',
     id: 16,
-    points: 2
+    points: 2,
+    type: ['all']
   },
   {
     name: 'Ostatní ustanovení',
     id: 25,
-    points: 2
+    points: 2,
+    type: ['all']
   },
   {
     name: 'Dopravní značky',
     id: 14,
-    points: 1
+    points: 1,
+    type: ['all']
   },
   {
     name: 'Řešení dopravních situací',
     id: 17,
-    points: 4
+    points: 4,
+    type: ['all']
   },
   {
     name: 'Zásady bezpečné jízdy [A]',
     id: 23,
-    points: 2
+    points: 2,
+    type: ['a']
   },
   {
     name: 'Zásady bezpečné jízdy [B]',
     id: 19,
-    points: 2
+    points: 2,
+    type: ['b']
   },
   {
     name: 'Zásady bezpečné jízdy [C,D]',
     id: 18,
-    points: 2
+    points: 2,
+    type: ['c', 'd']
   },
   {
     name: 'Související předpisy',
     id: 21,
-    points: 2
+    points: 2,
+    type: ['all']
   },
   {
     name: 'Podmínky provozu vozidel',
     id: 22,
-    points: 1
+    points: 1,
+    type: ['all']
   },
   {
     name: 'Zdravotnická příprava',
     id: 20,
-    points: 1
+    points: 1,
+    type: ['all']
   }
 ]
 
