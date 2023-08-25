8ae2889039e7f4be6ace91057f26544a96497b55
TomixUG <tomas.sousek1@gmail.com>
2023-08-25 14:39:24 +0200

    add questionsToExam

--- src/lectures.js
+++ src/lectures.js
@@ -4,67 +4,78 @@ const lectures = [
     name: 'Pojmy, povinnosti',
     id: 24,
     points: 2,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 2
   },
   {
     name: 'Jízda vozidly',
     id: 16,
     points: 2,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 4
   },
   {
     name: 'Ostatní ustanovení',
     id: 25,
     points: 2,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 4
   },
   {
     name: 'Dopravní značky',
     id: 14,
     points: 1,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 3
   },
   {
     name: 'Řešení dopravních situací',
     id: 17,
     points: 4,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 3
   },
   {
     name: 'Zásady bezpečné jízdy [A]',
     id: 23,
     points: 2,
-    type: ['a']
+    type: ['a'],
+    questionsToExam: 4
   },
   {
     name: 'Zásady bezpečné jízdy [B]',
     id: 19,
     points: 2,
-    type: ['b']
+    type: ['b'],
+    questionsToExam: 4
   },
   {
     name: 'Zásady bezpečné jízdy [C,D]',
     id: 18,
     points: 2,
-    type: ['c', 'd']
+    type: ['c', 'd'],
+    questionsToExam: 4
   },
   {
     name: 'Související předpisy',
     id: 21,
     points: 2,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 2
   },
   {
     name: 'Podmínky provozu vozidel',
     id: 22,
     points: 1,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 2
   },
   {
     name: 'Zdravotnická příprava',
     id: 20,
     points: 1,
-    type: ['a', 'b', 'c', 'd'] // all
+    type: ['a', 'b', 'c', 'd'], // all
+    questionsToExam: 1
   }
 ]
 
